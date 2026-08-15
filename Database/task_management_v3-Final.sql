250548edb71d808304b12b4f86c48cb7f8b9d01c
Krushna Khetia <krushnakhetia07@gmail.com>
2026-08-15 10:34:14 +0530

    Add files via upload

---
+++ Database/task_management_v3-Final.sql
@@ -0,0 +1,268 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 06, 2026 at 08:52 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `task_management_v3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Categories`
+--
+
+CREATE TABLE `Categories` (
+  `cat_id` int(11) NOT NULL,
+  `cat_name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Notifications`
+--
+
+CREATE TABLE `Notifications` (
+  `notif_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `msg` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `System_Logs`
+--
+
+CREATE TABLE `System_Logs` (
+  `log_id` int(11) NOT NULL,
+  `action` varchar(255) DEFAULT NULL,
+  `log_time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Tasks`
+--
+
+CREATE TABLE `Tasks` (
+  `task_id` int(11) NOT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `priority` varchar(20) DEFAULT NULL,
+  `est_hours` int(11) DEFAULT 0,
+  `est_minutes` int(11) DEFAULT 0,
+  `deadline` datetime DEFAULT NULL,
+  `cat_id` int(11) DEFAULT NULL,
+  `status_id` int(11) DEFAULT 1,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Triggers `Tasks`
+--
+DELIMITER $$
+CREATE TRIGGER `trg_task_completed` AFTER UPDATE ON `Tasks` FOR EACH ROW BEGIN
+    IF NEW.status_id = 3 AND OLD.status_id <> 3 THEN
+        INSERT INTO System_Logs(action)
+        VALUES (
+            CONCAT('Task ID ', NEW.task_id, ' marked as Completed')
+        );
+    END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Task_Progress`
+--
+
+CREATE TABLE `Task_Progress` (
+  `progress_id` int(11) NOT NULL,
+  `task_id` int(11) DEFAULT NULL,
+  `hours_spent` int(11) DEFAULT 0,
+  `minutes_spent` int(11) DEFAULT 0,
+  `entry_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Task_Status`
+--
+
+CREATE TABLE `Task_Status` (
+  `status_id` int(11) NOT NULL,
+  `status_name` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Task_Status`
+--
+
+INSERT INTO `Task_Status` (`status_id`, `status_name`) VALUES
+(1, 'Pending'),
+(2, 'In-Progress'),
+(3, 'Completed'),
+(4, 'Overdue');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE `Users` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `role` varchar(50) DEFAULT 'User',
+  `phone_no` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Users`
+--
+
+INSERT INTO `Users` (`user_id`, `name`, `username`, `password`, `role`, `phone_no`) VALUES
+(1, 'Master Admin', 'admin', 'admin123', 'Admin', '9000000000');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Categories`
+--
+ALTER TABLE `Categories`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `Notifications`
+--
+ALTER TABLE `Notifications`
+  ADD PRIMARY KEY (`notif_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `System_Logs`
+--
+ALTER TABLE `System_Logs`
+  ADD PRIMARY KEY (`log_id`);
+
+--
+-- Indexes for table `Tasks`
+--
+ALTER TABLE `Tasks`
+  ADD PRIMARY KEY (`task_id`),
+  ADD KEY `cat_id` (`cat_id`),
+  ADD KEY `status_id` (`status_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `Task_Progress`
+--
+ALTER TABLE `Task_Progress`
+  ADD PRIMARY KEY (`progress_id`),
+  ADD KEY `task_id` (`task_id`);
+
+--
+-- Indexes for table `Task_Status`
+--
+ALTER TABLE `Task_Status`
+  ADD PRIMARY KEY (`status_id`);
+
+--
+-- Indexes for table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Categories`
+--
+ALTER TABLE `Categories`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Notifications`
+--
+ALTER TABLE `Notifications`
+  MODIFY `notif_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `System_Logs`
+--
+ALTER TABLE `System_Logs`
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Tasks`
+--
+ALTER TABLE `Tasks`
+  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Task_Progress`
+--
+ALTER TABLE `Task_Progress`
+  MODIFY `progress_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Users`
+--
+ALTER TABLE `Users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Notifications`
+--
+ALTER TABLE `Notifications`
+  ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `Tasks`
+--
+ALTER TABLE `Tasks`
+  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`cat_id`) REFERENCES `Categories` (`cat_id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`status_id`) REFERENCES `Task_Status` (`status_id`),
+  ADD CONSTRAINT `tasks_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `Users` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `Task_Progress`
+--
+ALTER TABLE `Task_Progress`
+  ADD CONSTRAINT `task_progress_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `Tasks` (`task_id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
